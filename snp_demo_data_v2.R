56ea7adcbc7d175271400dec99a096f8de4c2b0c
Nate Olson <nathandavidolson@gmail.com>
2014-08-19 13:00:36 -0400

    adding supplemental files

---
+++ snp_demo_data_v2.R
@@ -0,0 +1,62 @@
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
+##
+## Demostration figures for variant calling algorithm validation review
+##
+## written by: Nate Olson
+## NIST
+## June 2014
+## modified to try and incorporate uncertainties
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
+
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(stringr)
+
+setwd("~/Desktop/snp_vcfs/")
+
+
+
+#######
+## Import of vcf files from GATK variant caller
+## input file names need to be the following format or change the input loop below
+## ORG_LAB_PLATFORM_pileup.expand.tsv
+#######
+
+snp_vcf_files <- list.files()[grep("^S.*vcf$", c(list.files()), value = F)]
+
+#%#%#% generating a dataframe combining the pileup parse files
+parse_vcf <- function(file){
+    fileTable <- read.table(file, header = F, sep = "\t")
+    colnames(fileTable) <- c("Control", "Location", "ID", "reference","alternative","qual", 
+                             "filter","INFO","FORMAT","other")
+    fileTable$dataset <- file
+    return(fileTable)
+}
+snp_calls <- ldply(snp_vcf_files,.fun = parse_vcf)
+
+
+snp_calls$dataset <- gsub("_L001_","",snp_calls$dataset)
+snp_calls$dataset <- gsub("R1_001","",snp_calls$dataset)
+head(snp_calls)
+library(plyr)
+library(stringr)
+# defining ture variants as variants with median quality scores of greater than 2000
+snp_summary <- ddply(snp_calls, .(Control, Location), summarize, count = length(dataset), medqual = median(qual))
+snp_summary$ref_call <- 1
+snp_summary$ref_call[snp_summary$medqual < 3000] <- 0
+#resulting in 30 positive and 89 negatives
+
+snp_calls$dataset <- str_replace(snp_calls$dataset,".vcf",replacement="")
+snp_calls$dataset <- str_replace(snp_calls$dataset,".sort",replacement="")
+snp_calls$dataset <- str_replace(snp_calls$dataset,".realign",replacement="")
+datasets_keep <- unique(snp_calls$dataset)[c(9,10,31,32)]# [c(1,9,18,27)]
+snp_calls <- subset(snp_calls[snp_calls$dataset %in% datasets_keep,], 
+                    select = c(Control, Location, qual, dataset))
+#setting max value of qual to 5000
+snp_calls$qual[snp_calls$qual > 5000] <- 5000
+snp_calls <- dcast(snp_calls, Control*Location~dataset, value.var="qual", fill=0)
+snp_calls <- join(snp_calls, snp_summary)
+snp_calls <- subset(snp_calls, select = -c(count,medqual, Control, Location))
+# Need to figure out how to incorporate the random and perfect datasets into the ROCR analysis
+write.csv(snp_calls, "~/Desktop/snp_demo_data.csv", row.names=F)
